bd8808522edaec40ac554ff6c37e9ed59fa7e8cb
Dovudbek <154534147+dovudbek2003@users.noreply.github.com>
2024-10-05 11:27:27 +0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
-  app.useBodyParser('json');
+  app.useBodyParser('json'); // comment
 
   // Nimadir sjdhfsjdh
 
