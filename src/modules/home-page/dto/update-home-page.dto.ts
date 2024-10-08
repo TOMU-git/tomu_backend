0ffdd75feee44a8b8f96c976ab7e1cb8d78d72aa
Hamidjon03 <forevalm@gmail.com>
2024-10-08 12:47:56 +0500

    delete home-page module from user-course branch

--- src/modules/home-page/dto/update-home-page.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateHomePageDto } from './create-home-page.dto';
-
-export class UpdateHomePageDto extends PartialType(CreateHomePageDto) {}
--- src/modules/home-page/home-page.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { HomePageService } from './home-page.service';
-import { HomePageController } from './home-page.controller';
-
-@Module({
-  controllers: [HomePageController],
-  providers: [HomePageService],
-})
-export class HomePageModule {}
