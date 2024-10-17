81a86e9852baf580416c27ba0d1b115f32a2d5c3
1lyosbek <ilyosxonisaqov007@gmail.com>
2024-10-17 06:09:02 +0000

    updated

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,6 @@ import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { RolesGuard } from './modules/shared/guards/role.guard';
 import { AuthGuard } from './modules/shared/guards/auth.guard';
-import bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -16,9 +15,6 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
-
-  app.use(bodyParser.json({ limit: '1000mb' }));
-  app.use(bodyParser.urlencoded({ limit: '1000mb', extended: true }));
 
   const httpAdapterHost = app.get(HttpAdapterHost);
   app.useGlobalFilters(new AllExceptionsFilter(httpAdapterHost));
