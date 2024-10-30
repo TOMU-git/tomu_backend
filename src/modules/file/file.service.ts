b8651f4bb0e9f6154361d1fbeb4c7dbe5ab57229
Hamidjon03 <forevalm@gmail.com>
2024-10-30 13:11:24 +0500

    updated path when upload file in file.service

--- src/modules/file/file.service.ts
+++ src/modules/file/file.service.ts
@@ -18,7 +18,7 @@ export class FileService {
     created.mimetype = createFileDto.mimetype;
     created.originalname = createFileDto.originalname;
     created.size = createFileDto.size;
-    created.path = `https://lms.ilyosbekdev.uz/${createFileDto.path}`;
+    created.path = `https://tomubackend.tomu.uz/$%7BcreateFileDto.path%7D`;
     const newData = await this.fileRepository.create(created);
     return new ResData<File>("File was created successfully", 201, newData);
   }
