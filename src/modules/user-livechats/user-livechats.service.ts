4efcd747e263b227362096d477aa5b139b02f7f3
Ilyosbek <ilyosxonisaqov007@gmail.com>
2024-11-13 18:40:50 +0500

    updated

--- src/modules/user-livechats/user-livechats.service.ts
+++ src/modules/user-livechats/user-livechats.service.ts
@@ -21,6 +21,7 @@ export class UserLivechatsService implements IUserLiveChatService {
     const newUserLiveChat = new UserLivechatEntity();
     newUserLiveChat.liveChatId = foundLiveChat.id;
     newUserLiveChat.teacherId = dto.teacherId;
+    newUserLiveChat.userId = newUserLiveChat.userId;
     newUserLiveChat.courseId = foundLiveChat.selectedCourseId;
     newUserLiveChat.isAccepted = true;
     newUserLiveChat.meetingDate = foundLiveChat.selectedDay;
